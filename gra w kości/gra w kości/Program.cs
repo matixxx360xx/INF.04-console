84bdbe7f3f93cba5a057591ec31641510c3e00f0
MSI\mati <matimichalski01@gmail.com>
2025-09-30 20:25:30 +0200

    gra w kości

--- gra w kości/gra w kości/Program.cs
+++ gra w kości/gra w kości/Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Kostka
+{
+
+    class Losuj
+    {
+        Random r = new Random();
+        public int[] tab;
+        public Losuj(int ile)
+        {
+            tab = new int[ile];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = r.Next(1, 7);
+            }
+        }
+    }
+
+    class Wyswietl
+    {
+
+        public void wyswietl(int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int j = i+1;
+                Console.WriteLine("Kostka "+j+": "+tab[i] + "\n");
+            }
+           
+        }
+    }
+
+    class Pkt {
+        public int suma;
+        Dictionary<int, int> liczbaWystapien = new Dictionary<int, int>();
+        public Pkt(int[] tab){
+            for(int i = 0; i < tab.Length; i++){
+                if (liczbaWystapien.ContainsKey(tab[i]))
+                {
+                    liczbaWystapien[tab[i]]++;
+                }
+                else {
+                    liczbaWystapien.Add(tab[i], 1);
+                }
+            }
+
+            var klucze = new List<int>(liczbaWystapien.Keys);
+            for (int i = 0; i < klucze.Count; i++)
+            {
+                if (liczbaWystapien[klucze[i]] > 1)
+                {
+                    suma += klucze[i] * liczbaWystapien[klucze[i]];
+                }
+            }
+            
+
+        }
+        public int Suma() {
+            return suma;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+
+            Wyswietl wyswietl = new Wyswietl();
+      
+            while (true)
+            {
+                Console.WriteLine("Ile kostek chcesz rzucić? (3-10)");
+                string? ile = Console.ReadLine();
+                if (!int.TryParse(ile, out int n) || n < 3 || n > 10)
+                {
+                    continue;
+                }
+                else
+                {
+                    Losuj l = new Losuj(int.Parse(ile));
+                    wyswietl.wyswietl(l.tab);
+                    Pkt p = new Pkt(l.tab);
+                    Console.WriteLine(p.Suma());
+                    Console.WriteLine("Jeszcze raz (t,n)");
+                    string? option;
+                    option = Console.ReadLine()?.Trim().ToLower();
+                    while (option != "t" && option != "n")
+                    {
+                        Console.WriteLine("Jeszcze raz (t,n)");
+                        option = Console.ReadLine()?.Trim().ToLower();
+                    }
+                    if (option == "t")
+                    {
+                        continue;
+                    }
+                    else if (option == "n")
+                    {
+                        break;
+                    }
+                    
+                }
+            }
+        }
+    }
+
+}
