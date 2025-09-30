0c413fe2f9a70b1440f73d8baa71c58541158d09
MSI\mati <matimichalski01@gmail.com>
2025-09-30 15:33:37 +0200

    Dodaj pliki projektów.

---
+++ wartownik/wartownik/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Zadanie
+{
+    public int[] tab = new int[50];
+
+    public void WypelnienieTablicy()
+    {
+        Random r = new Random();
+        for (int i = 0; i < tab.Length; i++)
+        {
+            tab[i] = r.Next(1, 101);
+        }
+    }
+    public void WyswietlenieTablicy()
+    {
+
+        Console.WriteLine(string.Join(",", tab));
+    }
+
+    public void znajdz(int i)
+    {
+        int j = 0;
+        int[] NewTab = new int[tab.Length + 1];
+        Array.Copy(tab, NewTab, tab.Length);
+        NewTab[NewTab.Length - 1] = i;
+        while (NewTab[j] != i)
+        {
+            j++;
+        }
+        if (j < tab.Length)
+        {
+            Console.WriteLine($"Znaleziono liczbę {i} na indeksie {j}");
+        }
+        else
+        {
+
+            Console.WriteLine("Nie znaleziono!");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Zadanie z = new Zadanie();
+        z.WypelnienieTablicy();
+        z.WyswietlenieTablicy();
+        z.znajdz(4);
+
+    }
+}
