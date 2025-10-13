1edcaf94cfc332feb48aed05838b524c96254cf7
MSI\mati <matimichalski01@gmail.com>
2025-10-13 20:01:21 +0200

    Filmy

---
+++ Filmy/Filmy/Program.cs
@@ -0,0 +1,34 @@
+﻿class Filmy {
+
+    private int wypozyczenia;
+    private string tytul;
+
+    public Filmy() {
+        wypozyczenia = 0;
+        tytul = "";
+    }
+
+    public void Setter_Tytul(string Tytul){
+       tytul = Tytul;
+    }
+    public string Getter_Tytul(){
+        return tytul;
+    }
+    public int Getter_Wypozycznia(){
+        return wypozyczenia;
+    }
+
+    public void Wypozyczenia(){
+        wypozyczenia++;
+    }
+
+    public static void Main() {
+        Filmy filmy = new();
+        Console.WriteLine(filmy.Getter_Wypozycznia());
+        filmy.Setter_Tytul("Pan Tadeusz");
+        filmy.Wypozyczenia();
+        Console.WriteLine(filmy.Getter_Tytul());
+        Console.WriteLine(filmy.Getter_Wypozycznia());
+
+    }
+}
---
+++ Konstruktory/Konstruktory/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+class Osoba
+{
+    public static int licznik = 0; 
+
+
+        private int id;
+        private string imie;
+
+    public Osoba() {
+        id = 0;
+        imie = " ";
+        licznik += 1;
+    }
+        
+
+
+
+    public Osoba(int id, string imie) {
+        this.id = id;
+        this.imie = imie;
+        licznik += 1;
+
+    }
+
+    public Osoba(Osoba osoba) {
+        this.id = osoba.id;
+        this.imie = osoba.imie;
+        licznik += 1;
+    }
+    public void wyswietl(string name) {
+        if (string.IsNullOrWhiteSpace(imie))
+        {
+            Console.WriteLine("Brak danych");
+        }
+        else {
+            Console.WriteLine($"Cześć {name}, mam na imię {imie}");
+        }
+    
+    }
+
+
+
+        static void Main() {
+            Osoba osoba = new Osoba();
+            Console.WriteLine("Podaj id");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj imie");
+            string imie = Console.ReadLine();
+
+            Osoba osoba2 = new Osoba(id,imie);
+            Osoba osoba3 = new Osoba(osoba2);
+            osoba.wyswietl("Jan");
+            osoba2.wyswietl("Jan");
+            osoba3.wyswietl("Jan");
+            Console.WriteLine(Osoba.licznik);
+          
+
+
+
+
+
+
+
+        
+    
+    
+    }
+
+
+
+
+
+
+}
